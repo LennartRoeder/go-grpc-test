5e4c7082ef463a86b93866b2a7c45d671a5cd54c
Lennart K <lennart.karsten@haw-hamburg.de>
2017-04-03 16:03:50 +0200

    gRPC tutorial

---
+++ client.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"flag"
+
+	"google.golang.org/grpc"
+
+	"log"
+
+	"io"
+
+	"math/rand"
+	"time"
+
+	pb "github.com/thinking-aloud/go-grpc-test/routes"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/grpclog"
+)
+
+var serverAddr = flag.String("server_addr", "127.0.0.1:10000", "The server address in the format of host:port")
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	if err != nil {
+		grpclog.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+
+	// Looking for a valid feature
+	printFeature(client, &pb.Point{Latitude: 409146138, Longitude: -746188906})
+
+	// Feature missing.
+	printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
+
+	// Looking for features between 40, -75 and 42, -73.
+	printFeatures(client, &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000},
+		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
+	})
+
+	// RecordRoute
+	runRecordRoute(client)
+
+	// RouteChat
+	runRouteChat(client)
+
+}
+
+func printFeature(client pb.RouteGuideClient, point *pb.Point) {
+	feature, err := client.GetFeature(context.Background(), point)
+	if err != nil {
+		grpclog.Fatalf("fail to get feature: %v", err)
+	}
+
+	log.Println(feature)
+}
+
+func printFeatures(client pb.RouteGuideClient, rect *pb.Rectangle) {
+	stream, err := client.ListFeatures(context.Background(), rect)
+	if err != nil {
+		grpclog.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+
+	}
+	for {
+		feature, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+		}
+		log.Println(feature)
+	}
+}
+
+func runRecordRoute(client pb.RouteGuideClient) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	pointCount := int(r.Int31n(100)) + 2 // Traverse at least two points
+	var points []*pb.Point
+	for i := 0; i < pointCount; i++ {
+		points = append(points, randomPoint(r))
+	}
+	log.Printf("Traversing %d points.", len(points))
+	stream, err := client.RecordRoute(context.Background())
+	if err != nil {
+		log.Fatalf("%v.RecordRoute(_) = _, %v", client, err)
+	}
+	for _, point := range points {
+		if err := stream.Send(point); err != nil {
+			log.Fatalf("%v.Send(%v) = %v", stream, point, err)
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+	}
+	log.Printf("Route summary: %v", reply)
+}
+
+func randomPoint(r *rand.Rand) *pb.Point {
+	lat := (r.Int31n(180) - 90) * 1e7
+	long := (r.Int31n(360) - 180) * 1e7
+	return &pb.Point{Latitude: lat, Longitude: long}
+}
+
+func runRouteChat(client pb.RouteGuideClient) {
+	notes := []*pb.RouteNote{
+		{&pb.Point{Latitude: 0, Longitude: 1}, "First message"},
+		{&pb.Point{Latitude: 0, Longitude: 2}, "Second message"},
+		{&pb.Point{Latitude: 0, Longitude: 3}, "Third message"},
+		{&pb.Point{Latitude: 0, Longitude: 1}, "Fourth message"},
+		{&pb.Point{Latitude: 0, Longitude: 2}, "Fifth message"},
+		{&pb.Point{Latitude: 0, Longitude: 3}, "Sixth message"},
+	}
+
+	stream, err := client.RouteChat(context.Background())
+	if err != nil {
+		grpclog.Fatalf("%v.RouteChat(_) = _, %v", client, err)
+	}
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				// read done.
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Failed to receive a note : %v", err)
+			}
+			log.Printf("Got message %s at point(%d, %d)", in.Message, in.Location.Latitude, in.Location.Longitude)
+		}
+	}()
+	for _, note := range notes {
+		if err := stream.Send(note); err != nil {
+			log.Fatalf("Failed to send a note: %v", err)
+		}
+	}
+	stream.CloseSend()
+	<-waitc
+}
